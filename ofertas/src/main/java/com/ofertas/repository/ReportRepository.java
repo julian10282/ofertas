efc1952b9bae0c5e7717bcb001f6c8ece6290268
Jhon Fredy Cuevas Martin <jhonfcuevas@gmail.com>
2018-10-12 20:50:59 -0500

    Ajuste en datos del reporte

--- ofertas/src/main/java/com/ofertas/repository/ReportRepository.java
+++ ofertas/src/main/java/com/ofertas/repository/ReportRepository.java
@@ -15,50 +15,50 @@ public class ReportRepository  {
         List<ReportEntity> result = new ArrayList<>();
 
         ReportEntity reportEntity = new ReportEntity();
-        reportEntity.setMes("Enero");
-        reportEntity.setNumOferta(32);
+        reportEntity.setMes("Marzo");
+        reportEntity.setNumOferta(38);
 
         result.add(reportEntity);
 
         ReportEntity reportEntity1 = new ReportEntity();
-        reportEntity1.setMes("Febrero");
-        reportEntity1.setNumOferta(32);
+        reportEntity1.setMes("Abril");
+        reportEntity1.setNumOferta(56);
 
         result.add(reportEntity1);
 
         ReportEntity reportEntity2 = new ReportEntity();
-        reportEntity2.setMes("Marzo");
-        reportEntity2.setNumOferta(32);
+        reportEntity2.setMes("Mayo");
+        reportEntity2.setNumOferta(94);
 
         result.add(reportEntity2);
 
         ReportEntity reportEntity3 = new ReportEntity();
-        reportEntity3.setMes("Abril");
-        reportEntity3.setNumOferta(32);
+        reportEntity3.setMes("Junio");
+        reportEntity3.setNumOferta(72);
 
         result.add(reportEntity3);
 
         ReportEntity reportEntity4 = new ReportEntity();
-        reportEntity4.setMes("Mayo");
-        reportEntity4.setNumOferta(32);
+        reportEntity4.setMes("Julio");
+        reportEntity4.setNumOferta(68);
 
         result.add(reportEntity4);
 
         ReportEntity reportEntity5 = new ReportEntity();
-        reportEntity5.setMes("Junio");
-        reportEntity5.setNumOferta(32);
+        reportEntity5.setMes("Agosto");
+        reportEntity5.setNumOferta(87);
 
         result.add(reportEntity5);
 
         ReportEntity reportEntity6 = new ReportEntity();
-        reportEntity6.setMes("Julio");
-        reportEntity6.setNumOferta(32);
+        reportEntity6.setMes("Septimbre");
+        reportEntity6.setNumOferta(96);
 
         result.add(reportEntity6);
 
         ReportEntity reportEntity7 = new ReportEntity();
-        reportEntity7.setMes("Agosto");
-        reportEntity7.setNumOferta(32);
+        reportEntity7.setMes("Octubre");
+        reportEntity7.setNumOferta(27);
 
         result.add(reportEntity7);
 
